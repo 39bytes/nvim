ecd2b1bd4162daa3ceec973e232373cdaf259fbd
JeffDotPng <zhangjeff2014@gmail.com>
2023-07-08 18:29:51 -0400

    initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,17 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local function map(mode, lhs, rhs, opts)
+  local options = { silent = true, noremap = true }
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map("n", "<leader>xx", "<cmd>TroubleToggle<cr>")
+map("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>")
+map("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>")
+map("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>")
+map("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>")
---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,43 @@
+local picker_layout = {
+  prompt_position = "top",
+  preview_cutoff = 10,
+  preview_width = 0.5,
+}
+
+return {
+  -- change some telescope options and a keymap to browse plugin files
+  {
+    "nvim-telescope/telescope.nvim",
+    -- change some options
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        file_ignore_patterns = { ".ipynb" },
+        path_display = { "smart" },
+      },
+      pickers = {
+        find_files = {
+          layout_config = picker_layout,
+        },
+        git_files = {
+          layout_config = picker_layout,
+        },
+        live_grep = {
+          layout_config = picker_layout,
+        },
+      },
+    },
+  },
+
+  -- add telescope-fzf-native
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+}
