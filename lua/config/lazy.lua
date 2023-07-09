540bbac4da45f9f077d05e8f9c347dd8a63bf3bd
JeffDotPng <zhangjeff2014@gmail.com>
2023-07-09 18:02:40 -0400

    remove copilot extra

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,7 +10,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    -- { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.rust" },
