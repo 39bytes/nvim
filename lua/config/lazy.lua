5be0fbd9dbef9684f8c665e4c4a8b986b7e11602
JeffDotPng <zhangjeff2014@gmail.com>
2023-11-02 12:50:50 -0400

    update

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -10,15 +10,6 @@ require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
-    { import = "lazyvim.plugins.extras.coding.yanky" },
-    { import = "lazyvim.plugins.extras.lang.python" },
-    { import = "lazyvim.plugins.extras.lang.rust" },
-    { import = "lazyvim.plugins.extras.lang.typescript" },
-    { import = "lazyvim.plugins.extras.lang.tailwind" },
-    { import = "lazyvim.plugins.extras.lang.json" },
-    { import = "lazyvim.plugins.extras.linting.eslint" },
-    { import = "lazyvim.plugins.extras.formatting.prettier" },
-    -- { import = "lazyvim.plugins.extras.lang.clangd" },
     -- import any extras modules here
     -- { import = "lazyvim.plugins.extras.lang.typescript" },
     -- { import = "lazyvim.plugins.extras.lang.json" },
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -2,8 +2,8 @@ return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
     vim.list_extend(opts.sources, {
-      require("null-ls").builtins.formatting.yapf,
-      -- require("null-ls").builtins.formatting.black,
+      require("null-ls").builtins.formatting.ruff,
+      -- require("null-ls").builtins.formatting.prettier,
       -- require("null-ls").builtins.formatting.ocamlformat,
     })
   end,
