5b1aa7bdf150b6f41f3d43b09d88f3a6472f2ecc
JeffDotPng <zhangjeff2014@gmail.com>
2024-03-28 19:58:51 -0400

    disable relative line numberse

--- lua/options.lua
+++ lua/options.lua
@@ -7,12 +7,13 @@
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
-vim.opt.relativenumber = true
+-- vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
+-- Don't wrap lines
 vim.opt.wrap = false
 
 -- Enable mouse mode, can be useful for resizing splits for example!
