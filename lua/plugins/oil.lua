78cf6c88c798f25e43265fbf5caa98ddf6ad206b
JeffDotPng <zhangjeff2014@gmail.com>
2024-04-16 01:45:06 -0400

    fix oil keymaps

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,9 +4,6 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
     require('oil').setup {}
-    vim.keymap.set('n', '<leader>oo', '<CMD>Oil<CR>', { desc = 'Open oil' })
-    vim.keymap.set('n', '<leader>oc', function()
-      require('oil').open(vim.fn.getcwd())
-    end, { desc = 'Open oil in CWD' })
+    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open oil' })
   end,
 }
